e5d9f440e9aa929f3aef328da90f9c33f5353ba2
lizhequ <lizhequ@ysbang.cn>
2024-09-25 17:28:48 +0800

    portal demo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, View } from '@tamagui/core'
+
+import config from './tamagui.config' // your configuration
+import { Button, Portal, PortalProvider, Text } from 'tamagui'
+import { useState } from 'react'
+
+function Portal1() {
+  return (
+    <Portal>
+      <View mt={100}>
+        <Text>portal1 content</Text>
+      </View>
+    </Portal>
+  )
+}
+
+function Portal2() {
+  return (
+    <Portal>
+      <View mt={200}>
+        <Text>portal2 content</Text>
+      </View>
+    </Portal>
+  )
+}
+
+function Demo() {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <View pt={400}>
+      <Text>toggle value: {String(toggle)}</Text>
+      <Button mt={10} onPress={() => setToggle(!toggle)}>
+        toggle
+      </Button>
+      {toggle ? <Portal1 /> : <Portal2 />}
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TamaguiProvider config={config}>
+      <PortalProvider shouldAddRootHost>
+        <Demo />
+      </PortalProvider>
+    </TamaguiProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
